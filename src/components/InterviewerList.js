b4d96ff980ad1693d5b9b9e97344ea0e952c781b
Wahab Alamin <wahabalamin@gmail.com>
2021-09-30 22:45:54 -0400

    moved the onClick event handler to interviewerList from ListItem, and just call the setInterivewer where the event handler was originally. Updated the clickable interviewlistitem story.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "components/InterviewerList.scss"
+import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
 
+  const { interviewers, interviewer, setInterviewer } = props;
+
+  const parsedInterviewers = interviewers.map(eachInterviewer => <InterviewerListItem key={eachInterviewer.id} id={eachInterviewer.id} name={eachInterviewer.name} avatar={eachInterviewer.avatar} selected={eachInterviewer.id === interviewer} setInterviewer={(event) => setInterviewer(eachInterviewer.id)} />)
+
   return (
     <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <h4 className="interviewers__header text--light">interviewer</h4>
+      <ul className="interviewers__list">{ parsedInterviewers }</ul>
     </section>
   );
 }
