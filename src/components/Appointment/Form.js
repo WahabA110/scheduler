d2550d24b75770908cd57742e18bf001ddbf4d9c
Wahab Alamin <wahabalamin@gmail.com>
2021-10-02 17:45:28 -0400

    Updated files to use useState hooks to update the students name and the interviewer we’re looking to book an appointment with. Also updated the Save and Cancel buttons. Cancel will now reset the input field to be empty and remove the selected interviewer.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,36 +5,40 @@ import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Form(props) {
 
-  const { name, interviewers, interviewer, onSave, onCancel, setInterviewer } = props;
+  const { name, interviewers, interviewer, onSave, onCancel } = props;
 
   const [ StudentName, setStudentName ] = useState(name || "");
-  const [ InterviewerName, setInterviewerName ] = useState(interviewer || null);
+  const [ InterviewerId, setInterviewerId ] = useState(interviewer || null);
 
   const reset = () => {
-    
+    setStudentName("")
+    setInterviewerId(null)
+  }
+
+  const cancel = () => {
+    reset()
+    onCancel()
   }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={name}
-          /*
-            This must be a controlled component
-          */
+            value={StudentName}
+            onChange={(event) => setStudentName(event.target.value)}
           />
         </form>
-        <InterviewerList interviewers={interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
+        <InterviewerList interviewers={interviewers} interviewer={InterviewerId} setInterviewer={setInterviewerId} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
-          <Button confirm onClick={onSave}>Save</Button>
+          <Button danger onClick={() => cancel()}>Cancel</Button>
+          <Button confirm onClick={() => onSave(StudentName, InterviewerId)}>Save</Button>
         </section>
       </section>
     </main>
