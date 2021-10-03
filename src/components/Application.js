70087ce7337cc2cb988087da6bc4851fe3cc7663
Wahab Alamin <wahabalamin@gmail.com>
2021-10-03 19:56:41 -0400

    Added axis as a dependency. Imported useEffect and axis into file. Used axios to get api/days and used setDays with the data received from the url.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
@@ -54,28 +54,18 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const parsedAppointments = appointments.map(eachAppointment => <Appointment key={eachAppointment.id} {...eachAppointment} />)
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday")
+  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days")
+    .then(response => {
+      setDays([...response.data])
+    });
+  }, []);
 
   return (
     <main className="layout">
