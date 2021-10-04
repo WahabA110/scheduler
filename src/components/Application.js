c3e634013a7f2506349998228a8ebdfc899e4826
Wahab Alamin <wahabalamin@gmail.com>
2021-10-04 16:28:18 -0400

    Updated useState to be an object by default, and inputted the previous day and days states into there. Created a function for setDay and setDays to input a new state for only either the day or days state.

--- src/components/Application.js
+++ src/components/Application.js
@@ -24,7 +24,7 @@ const appointments = [
   {
     id: 3,
     time: "2pm"
-  },{
+  }, {
     id: 4,
     time: "3pm",
     interview: {
@@ -56,15 +56,23 @@ const appointments = [
 
 const parsedAppointments = appointments.map(eachAppointment => <Appointment key={eachAppointment.id} {...eachAppointment} />)
 
+
+
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+  })
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get("http://localhost:8001/api/days")
-    .then(response => {
-      setDays([...response.data])
-    });
+      .then(response => {
+        setDays([...response.data])
+      });
   }, []);
 
   return (
@@ -76,7 +84,7 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"><DayList days={days} day={day} setDay={setDay} /></nav>
+        <nav className="sidebar__menu"><DayList days={state.days} day={state.day} setDay={setDay} /></nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -84,7 +92,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        { parsedAppointments }
+        {parsedAppointments}
       </section>
     </main>
   );
