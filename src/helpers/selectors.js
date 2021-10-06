1409353036a9a67aa91c71cebfc103d02c301243
Wahab Alamin <wahabalamin@gmail.com>
2021-10-06 13:10:35 -0400

    added a getInterviewersForDay function and added tests to make sure it acts as expected

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,15 +1,29 @@
 
 export function getAppointmentsForDay(state, day) {
 
-  const foundDay = state.days.find(oneDay => oneDay.name === day)
+  const foundDay = state.days.find(oneDay => oneDay.name === day);
 
   if(state.days.length === 0 || foundDay === undefined) {
     return [];
   }
   
-  const foundAppointments = foundDay.appointments.map(id => state.appointments[id])
+  const foundAppointments = foundDay.appointments.map(id => state.appointments[id]);
 
   return foundAppointments;
+  
+}
+
+export function getInterviewersForDay(state, day) {
+
+  const foundDay = state.days.find(oneDay => oneDay.name === day);
+
+  if(state.days.length === 0 || foundDay === undefined) {
+    return [];
+  }
+
+  const foundInterviewers = foundDay.interviewers.map(id => state.interviewers[id]);
+
+  return foundInterviewers;
   
 }
 
