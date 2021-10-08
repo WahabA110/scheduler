dbc9b8c9e274e3aaaefa04191bd10517fa96687e
Wahab Alamin <wahabalamin@gmail.com>
2021-10-08 17:15:28 -0400

    added data-testid to the li element in this file for testing purposes

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -24,7 +24,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li className={dayClass} onClick={() => setDay(name)}>
+    <li data-testid="day" className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{spotsAvailable()}</h3>
     </li>
